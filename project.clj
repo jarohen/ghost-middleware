f4258e9213ee25ab43668a13975f79b8e6536925
James Henderson <james@pcr.me.uk>
2013-12-29 23:32:46 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jarohen/shebang-middleware "0.1.0"
+  :description "Some Ring middleware to handle Google's AJAX shebang spec"
+  :url "https://github.com/james-henderson/shebang.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
